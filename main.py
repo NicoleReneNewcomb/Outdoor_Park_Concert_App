d5fa04004a64373b32b13c9607560b0795121df9
Nicole-Rene Newcomb <112290345+Nicole-Rene-Newcomb@users.noreply.github.com>
2023-04-28 01:50:05 -0700

    Created main.py File with Description
    Created main.py file and described app in comments.

---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+"""
+    Student: Nicole-Rene Newcomb
+    Class: CIS024C Python Programming
+    Description: app manages concert seat availability
+        and ticket sales with receipts for users.
+"""
